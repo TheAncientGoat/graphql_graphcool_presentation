0c00855e802c205c37502dbbefe06b86671bb0d3
Ryan Swart <ryanswrt@gmail.com>
2017-06-04 12:46:51 +0800

    initialize project with react deps

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'                         
+import {ApolloProvider, createNetworkInterface, ApolloClient} from 'react-apollo'   
+
+ReactDOM.render(<h1> Hi </h1>, document.getElementById('root'));
